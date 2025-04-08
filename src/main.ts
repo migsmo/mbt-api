0c40b42310c1f483dc211d92db57253f2d65cb50
migsmo <97015261+migsmo@users.noreply.github.com>
2025-04-08 23:45:24 +0800

    add usage of loggers, filters, and allow localhost:3000 to access API

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './commons/filters/global-exception.filter';
+import { GlobalErrorLoggerService } from './commons/logger/global-error-logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Needed for setting httpOnly cookies in auth APIs
   app.use(cookieParser());
+  const loggerService = app.get(GlobalErrorLoggerService);
+  app.useGlobalFilters(new GlobalExceptionFilter(loggerService));
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
