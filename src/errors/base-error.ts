5f229fcbe0bfb0bee420b17491d901f42a53067f
migsmo <97015261+migsmo@users.noreply.github.com>
2025-04-08 23:44:43 +0800

    Create base-error.ts

---
+++ src/errors/base-error.ts
@@ -0,0 +1,11 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export class BaseError extends HttpException {
+  constructor(
+    public readonly clientMessage: string,
+    public readonly internalMessage?: string,
+    statusCode: number = HttpStatus.INTERNAL_SERVER_ERROR,
+  ) {
+    super(clientMessage, statusCode);
+  }
+}
