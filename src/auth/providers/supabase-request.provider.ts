19d153ac1bf36faa777785faa404df9e12548d35
migsmo <97015261+migsmo@users.noreply.github.com>
2025-04-13 17:19:31 +0800

    Create supabase request provider
    This is the same as supabase provider but this one sets the session so that we can make requests

---
+++ src/auth/providers/supabase-request.provider.ts
@@ -0,0 +1,47 @@
+import { Provider, Scope } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { REQUEST } from '@nestjs/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { Request } from 'express';
+import { SupabaseInitializationError } from 'src/errors/auth.error';
+
+export const SUPABASE_REQUEST_CLIENT = 'SUPABASE_REQUEST_CLIENT';
+
+export const SupabaseRequestProvider: Provider = {
+  provide: SUPABASE_REQUEST_CLIENT,
+  scope: Scope.REQUEST,
+  useFactory: async (
+    configService: ConfigService,
+    request: Request,
+  ): Promise<SupabaseClient> => {
+    const supabaseUrl = configService.get<string>('supabaseUrl');
+    const supabaseKey = configService.get<string>('supabaseKey');
+
+    if (supabaseUrl === undefined || supabaseKey === undefined) {
+      throw new SupabaseInitializationError('Check configuration settings.');
+    }
+
+    let client: SupabaseClient;
+
+    try {
+      client = createClient(supabaseUrl, supabaseKey);
+
+      const authHeader = request.headers.authorization;
+      if (authHeader && authHeader.startsWith('Bearer ')) {
+        const accessToken = authHeader.split(' ')[1];
+
+        await client.auth.setSession({
+          access_token: accessToken,
+          refresh_token: (request.cookies['refresh_token'] as string) || '',
+        });
+      }
+    } catch (error: any) {
+      throw new SupabaseInitializationError(
+        error instanceof Error ? error.message : '',
+      );
+    }
+
+    return client;
+  },
+  inject: [ConfigService, REQUEST],
+};
