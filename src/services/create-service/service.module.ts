f163c29e05591f4daad2f8a3cced63db91d49c50
migsmo <97015261+migsmo@users.noreply.github.com>
2025-04-13 17:21:44 +0800

    Update service.module.ts

--- src/services/create-service/service.module.ts
+++ src/services/create-service/service.module.ts
@@ -1,11 +1,12 @@
 // src/services/create-service.module.ts
 import { Module } from '@nestjs/common';
+import { SupabaseRequestProvider } from 'src/auth/providers/supabase-request.provider';
 import { SupabaseProvider } from 'src/auth/providers/supabase.provider';
 import { CreateServiceController } from './create-service.controller';
 import { CreateServiceService } from './create-service.service';
 
 @Module({
   controllers: [CreateServiceController],
-  providers: [CreateServiceService, SupabaseProvider],
+  providers: [CreateServiceService, SupabaseProvider, SupabaseRequestProvider],
 })
 export class CreateServiceModule {}
