17b26a51e7fd9724c682e0759f0ebc5e1e59069e
migsmo <97015261+migsmo@users.noreply.github.com>
2025-04-23 18:45:45 +0800

    Create appointments.entity.ts

---
+++ src/services/entity/appointments.entity.ts
@@ -0,0 +1,8 @@
+export class Appointment {
+  id: string;
+  created_at: string;
+  date_time: string;
+  additional_remarks: string;
+  selected_services: string[];
+  customer_assigned: string;
+}
