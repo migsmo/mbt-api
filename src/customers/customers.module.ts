21ceb8000e374a989975a9b6dc438a9583a58aef
migsmo <97015261+migsmo@users.noreply.github.com>
2025-04-26 22:13:43 +0800

    create get customer API

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { SupabaseRequestProvider } from 'src/auth/providers/supabase-request.provider';
 import { GetAllCustomersController } from './get-all-customers/get-all-customers.controller';
 import { GetAllCustomersService } from './get-all-customers/get-all-customers.service';
+import { GetCustomerController } from './get-customer/get-customer.controller';
+import { GetCustomerService } from './get-customer/get-customer.service';
 
 @Module({
-  controllers: [GetAllCustomersController],
-  providers: [GetAllCustomersService, SupabaseRequestProvider],
+  controllers: [GetAllCustomersController, GetCustomerController],
+  providers: [
+    GetAllCustomersService,
+    SupabaseRequestProvider,
+    GetCustomerService,
+  ],
 })
 export class CustomersModule {}
