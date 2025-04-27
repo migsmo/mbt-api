acd97f92a787ca127298adf015470403f6e9840d
migsmo <97015261+migsmo@users.noreply.github.com>
2025-04-27 17:31:05 +0800

    add sending of assigned staff to get appointment

--- src/appointments/get-appointment/get-appointment.service.ts
+++ src/appointments/get-appointment/get-appointment.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { SupabaseClient } from '@supabase/supabase-js';
 import { SUPABASE_REQUEST_CLIENT } from 'src/auth/providers/supabase-request.provider';
 import { Appointments } from 'src/entity/appointments.entity';
+import { AppointmentServices } from 'src/entity/appointmentService.entity';
 import { BaseError } from 'src/errors/base-error';
 import { GetAppointmentResponse } from './dto/get-appointment-response.dto';
 
@@ -27,12 +28,27 @@ export class GetAppointmentService {
       );
     }
 
+    const appointmentService = await this.supabase
+      .from('appointment_services')
+      .select('*')
+      .eq('appointment_id', appointmentData.id);
+
+    const appointmentServiceData =
+      appointmentService.data as AppointmentServices[];
+
+    const selectedServices = appointmentServiceData.map((service) => {
+      return {
+        serviceId: service.service_id,
+        staffIds: service.employee_ids,
+      };
+    });
+
     const response: GetAppointmentResponse = {
       id: appointmentData.id,
       createdAt: new Date(appointmentData.created_at),
       dateTime: new Date(appointmentData.date_time),
       additionalRemarks: appointmentData.additional_remarks,
-      selectedServices: appointmentData.selected_services,
+      selectedServices: selectedServices,
       customerAssigned: appointmentData.customer_assigned,
     };
 
