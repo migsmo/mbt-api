db85a101c75c3fcd1e79f519ad095ae7a60adb53
migsmo <97015261+migsmo@users.noreply.github.com>
2025-04-28 19:19:44 +0800

    Create employee API

--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { SupabaseRequestProvider } from 'src/auth/providers/supabase-request.provider';
+import { CreateEmployeeController } from './create-employee/create-employee.controller';
+import { CreateEmployeeService } from './create-employee/create-employee.service';
 import { GetAllEmployeesController } from './get-all-employees/get-all-employees.controller';
 import { GetAllEmployeesService } from './get-all-employees/get-all-employees.service';
 
 @Module({
-  controllers: [GetAllEmployeesController],
-  providers: [GetAllEmployeesService, SupabaseRequestProvider],
+  controllers: [GetAllEmployeesController, CreateEmployeeController],
+  providers: [
+    GetAllEmployeesService,
+    SupabaseRequestProvider,
+    CreateEmployeeService,
+  ],
 })
 export class EmployeesModule {}
