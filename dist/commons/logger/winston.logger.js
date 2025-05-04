6e341ffa7d5c5b1aab8b93c886b17a2039f3c20e
migsmo <97015261+migsmo@users.noreply.github.com>
2025-05-04 23:23:00 +0800

    new build

--- dist/commons/logger/winston.logger.js
+++ dist/commons/logger/winston.logger.js
@@ -5,6 +5,7 @@ const path = require("path");
 const winston_1 = require("winston");
 const { combine, timestamp, printf, colorize, errors } = winston_1.format;
 const dateStr = new Date().toISOString().split('T')[0];
+const isServerless = process.env.NODE_ENV === 'production';
 const logFormat = printf(({ message, timestamp, stack }) => {
     return `[${timestamp}]: ${message || ''}.\n ${stack}`;
 });
@@ -12,12 +13,16 @@ exports.winstonLogger = (0, winston_1.createLogger)({
     level: 'info',
     format: combine(colorize(), timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), errors({ stack: true }), logFormat),
     transports: [
-        new winston_1.transports.File({
-            filename: path.join(__dirname, `../../../logs/${dateStr}.log`),
-        }),
         new winston_1.transports.Console({
-            format: winston_1.format.combine(winston_1.format.colorize(), logFormat),
+            format: combine(colorize(), logFormat),
         }),
+        ...(!isServerless
+            ? [
+                new winston_1.transports.File({
+                    filename: path.join(__dirname, `../../../logs/${dateStr}.log`),
+                }),
+            ]
+            : []),
     ],
 });
 //# sourceMappingURL=winston.logger.js.map
