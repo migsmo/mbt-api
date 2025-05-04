6b4544c678974430624b938c9d21ddecc5cb2691
migsmo <97015261+migsmo@users.noreply.github.com>
2025-05-04 16:02:59 +0800

    Delete config.ts

--- src/config/config.ts
+++
@@ -1,6 +0,0 @@
-export default () => ({
-  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
-  supabaseKey: process.env.SUPABASE_ANON_KEY,
-  jwtSecret: process.env.JWT_SECRET,
-  port: 3000,
-});
