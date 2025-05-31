3e741108c39c11b8287eeb5c496ae82f0a5b6edb
migsmo <97015261+migsmo@users.noreply.github.com>
2025-05-31 11:20:08 +0800

    create update appointment billings api

--- src/appointment-billings/appointment-billings.module.ts
+++ src/appointment-billings/appointment-billings.module.ts
@@ -4,16 +4,20 @@ import { CreateAppointmentBillingService } from './create-appointment-billing/cr
 import { CreateAppointmentBillingController } from './create-appointment-billing/create-appointment-billings.controller';
 import { GetAllAppointmentBillingsController } from './get-all-appointment-billings/get-all-appointment-billings.controller';
 import { GetAllAppointmentBillingsService } from './get-all-appointment-billings/get-all-appointment-billings.service';
+import { UpdateAppointmentBillingsController } from './update-appointment-billings/update-appointment-billings.controller';
+import { UpdateAppointmentBillingsService } from './update-appointment-billings/update-appointment-billings.service';
 
 @Module({
   controllers: [
     CreateAppointmentBillingController,
     GetAllAppointmentBillingsController,
+    UpdateAppointmentBillingsController,
   ],
   providers: [
     SupabaseRequestProvider,
     CreateAppointmentBillingService,
     GetAllAppointmentBillingsService,
+    UpdateAppointmentBillingsService,
   ],
 })
 export class AppointmentBillingsModule {}
---
+++ src/appointment-billings/update-appointment-billings/dto/update-appointment-billings-request.dto.ts
@@ -0,0 +1,4 @@
+export class UpdateAppointmentBillingsRequest {
+  appointmentId: string;
+  billingIds: string[];
+}
